74e94b6d7d8c55925e57ac82a319ace449243608
dengwei2 <2546765805@qq.com>
2021-01-20 21:00:10 +0800

    提交

---
+++ src/main/java/com/dw/study/utils/RedisUtils.java
@@ -0,0 +1,175 @@
+package com.dw.study.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author
+ * @ClassName RedisUtils
+ * @Description
+ * @Date 2021/1/20 12:22
+ * @Version 1.0
+ */
+@Component
+public class RedisUtils {
+
+    private final static Logger log = LoggerFactory.getLogger(RedisUtils.class);
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    /**
+     * 设置缓存
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean set(String key, Object value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 设置值并设置过期时间（单位秒）
+     *
+     * @param key
+     * @param value
+     * @param time  过期时间
+     * @return
+     */
+    public boolean set(String key, Object value, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+            } else {
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 将旧的key设置为value，并且返回旧的key
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public Object getAndSet(String key, Object value) {
+        try {
+            Object andSet = redisTemplate.opsForValue().getAndSet(key, value);
+            return andSet;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * 如果不存在则设置值value，返回true。 否则返回false
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean setIfAbsent(String key, String value) {
+        try {
+            return redisTemplate.opsForValue().setIfAbsent(key, value);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 在原有的值基础上新增字符串到末尾
+     *
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean append(String key, String value) {
+        try {
+            redisTemplate.opsForValue().append(key, value);
+            return true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+
+
+    /**
+     * 获取value
+     *
+     * @param key
+     * @return
+     */
+    public Object get(String key) {
+        return key == null ? null : redisTemplate.opsForValue().get(key);
+    }
+
+
+    /**
+     * 删除缓存
+     *
+     * @param key
+     */
+    public void del(String... key) {
+        if (key != null && key.length > 0) {
+            if (key.length == 1) {
+                redisTemplate.delete(key[0]);
+            } else {
+                redisTemplate.delete(Arrays.asList(key));
+            }
+        }
+    }
+
+    /**
+     * 判断key是否存在
+     *
+     * @param key 键
+     * @return true 存在 false不存在
+     */
+    public boolean hasKey(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 根据key 获取过期时间
+     *
+     * @param key 键 不能为null
+     * @return 时间(秒) 返回-1, 代表为永久有效
+     */
+    public long getExpire(String key) {
+        return redisTemplate.getExpire(key, TimeUnit.SECONDS);
+    }
+
+
+
+}
