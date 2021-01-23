95f88febb64780af8503eaa22d267e069d6a1f95
dengwei <dengwei>
2021-01-23 21:49:34 +0800

    新增SpringCache

--- src/main/java/com/dw/study/config/RedisConfig.java
+++ src/main/java/com/dw/study/config/RedisConfig.java
@@ -1,23 +1,38 @@
 package com.dw.study.config;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
 
 /**
  * @Author
  * @ClassName RedisConfig
- * @Description
+ * @Description redis配置： 如果需要使用SpringCache 方法级别的缓存需要加上@EnableCaching开启对缓存的支持，
+ * 并且需要继承CachingConfigurerSupport以配置基于方法级别的缓存配置，如果只是单纯的使用Redis做缓存，不基于SpringCache框架，则不用配置
+ * 只需要配置Redis的序列化方式
  * @Date 2021/1/20 19:43
  * @Version 1.0
  */
 @Configuration
-public class RedisConfig {
+//@EnableCaching开启对SpringCache的支持（提供基于方法级别的缓存）
+@EnableCaching
+@Slf4j
+public class RedisConfig extends CachingConfigurerSupport{
 
     /**
      * 设置Redis序列化方式，默认使用的JDKSerializer的序列化方式，效率低，这里我们使用 FastJsonRedisSerializer
@@ -38,6 +53,63 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
+
+
+    /**
+     * 配置SpringCache基于方法级别的缓存，key的生成策略的配置: 类名+方法名+参数列表的类型+参数值 再做 哈希散列 作为key
+     *
+     * @return KeyGenerator
+     */
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        log.info("RedisCacheConfig.keyGenerator()");
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object o, Method method, Object... objects) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(o.getClass().getSimpleName())
+                        .append(":")
+                        .append(method.getName())
+                        .append(":");
+                for (Object obj : objects) {
+                    if (null != obj) {// 替换字符串
+                        String objKey = JSON.toJSONString(obj);
+                        objKey = objKey.replace(":", "=");
+                        sb.append(objKey);
+                    }
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+
+    /**
+     * 配置SpringCache基于方法级别的缓存的过期时间、key-value的序列化方式
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        //redis缓存配置
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                //缓存生存时间60秒
+                .entryTtl(Duration.ofSeconds(60))
+                // 配置Key序列化（解决乱码的问题）
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                // 配置Value序列化
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new FastJsonRedisSerializer<>(Object.class)))
+                // 不缓存空值
+                .disableCachingNullValues();
+        //根据redis缓存配置和redis连接工厂生成redis缓存管理器
+        RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+        log.debug("自定义RedisCacheManager加载完成");
+        return redisCacheManager;
+    }
+
 
 
 }
---
+++ src/main/java/com/dw/study/controller/TestCacheController.java
@@ -0,0 +1,59 @@
+package com.dw.study.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author dw
+ * @ClassName TestCacheController
+ * @Description 测试基于SpringCache做方法级别的缓存
+ * @Date 2021/1/23 21:16
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+public class TestCacheController {
+
+
+    /**
+     * @Cacheable 标注在方法上，如果该方法结果存在缓存则使用缓存，否则执行方法并将结果缓存
+     * @Cacheable 参数：cacheNames 被缓存的时候的命名空间
+     * key	这里的key的优先级是最高的，可以覆盖掉全局配置的key，如果不配置的话使用的就是全局的key
+     * keyGenerator	指定的缓存的key的生成器，如果没有在RedisConfig配置默认没有
+     * cacheManager	 指定要使用哪个缓存管理器。默认是底层自动配置的管理器（这里在RedisConfig中配置的RedisCacheManager）
+     * condition 满足什么条件会进行缓存，里面可以写简单的表达式进行逻辑判断: condition = "#count = 1"
+     * unless	满足什么条件不进行缓存，里面可以写简单的表达式进行逻辑判断
+     * sync	加入缓存的这个操作是否是同步的
+     * value 指定将方法的返回结果放在哪个缓存中，可以指定多个，用大括号保存
+     * @return
+     */
+    @RequestMapping("addCache")
+    @Cacheable(value = "001")
+    public String testAddCache(Integer count){
+        log.info("如果我被打印，并且count=2,说明没有走缓存");
+        count += 1;
+        return "count==" + count;
+    }
+
+    /**
+     *  @CacheEvict 清除缓存，
+     *  beforeInvocation 在执行删除方法前就执行清空缓存操作，默认是false，如果删除方法执行报错该注解则不执行
+     * allEntries	是否删除该命名空间下面的全部缓存，默认是false
+     * @param count
+     * @return
+     */
+    @RequestMapping("removeCache")
+    @CacheEvict
+    public String testRemoveCache(Integer count){
+        log.info("缓存已被删除");
+        count += 1;
+        return "缓存已被删除count==" + count;
+    }
+
+    
+
+
+}
